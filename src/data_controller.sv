ac19c54e28f2745aa604b993419657e063d909c6
Krzysztof Cislo <krzysztof.cislo@nokia.com>
2023-02-11 18:45:01 +0100

    changed data_controller clock to data_clock

--- src/data_controller.sv
+++ src/data_controller.sv
@@ -22,9 +22,9 @@
 import aurora_pkg::*;
 
 module data_controller(
-    input logic clk,
+    input logic clk,  // data clock
     input logic rst_n,
-    input logic single_lane, // FIXME: is it needed?
+    input logic single_lane,
     input logic axi_valid,
     input logic axi_last,
     input logic [AXI_DATA_SIZE-1:0] axi_data,
@@ -38,8 +38,12 @@ module data_controller(
         LAST
     } state_e;
 
-    logic axi_valid_prev;
     state_e state, state_nxt;
+
+    logic axi_valid_delayed;
+    logic axi_last_delayed;
+    logic [AXI_DATA_SIZE-1:0] axi_data_delayed;
+
     ordered_sets_e ordered_sets_nxt;
     logic [AXI_DATA_SIZE-1:0] data_out_nxt;
 
@@ -48,12 +52,16 @@ module data_controller(
             state <= IDLE;
             ordered_sets <= NONE;
             data_out <= '0;
-            axi_valid_prev <= '0;
+            axi_valid_delayed <= '0;
+            axi_last_delayed <= '0;
+            axi_data_delayed <= '0;
         end else begin
             state <= state_nxt;
             ordered_sets <= ordered_sets_nxt;
             data_out <= data_out_nxt;
-            axi_valid_prev <= axi_valid;
+            axi_valid_delayed <= axi_valid;
+            axi_last_delayed <= axi_last;
+            axi_data_delayed <= axi_data;
         end
     end
 
@@ -64,16 +72,16 @@ module data_controller(
 
         case (state)
             IDLE: begin
-                if (axi_valid & !axi_valid_prev) begin
+                if (axi_valid) begin
                     ordered_sets_nxt = SCP;
                     state_nxt = STREAM;
                 end
             end
             STREAM: begin
-                data_out_nxt = axi_data;
-                if (!axi_valid) begin
+                data_out_nxt = axi_data_delayed;
+                if (!axi_valid_delayed) begin
                     ordered_sets_nxt = I;
-                end else if (axi_last) begin
+                end else if (axi_last_delayed) begin
                     state_nxt = LAST;
                 end
             end
