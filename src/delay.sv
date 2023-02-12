a8e9b647784777ba056296197d37f013e733faa7
Krzysztof Cislo <krzysztof.cislo@nokia.com>
2023-02-12 02:46:17 +0100

    added header for delay

--- src/delay.sv
+++ src/delay.sv
@@ -1,3 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:
+// Engineer:
+//
+// Create Date: 12.02.2023 20:23:02
+// Design Name:
+// Module Name: delay
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+//
+//////////////////////////////////////////////////////////////////////////////////
+
 module delay
     #( parameter
         WIDTH   = 8, // bit width of the input/output data
